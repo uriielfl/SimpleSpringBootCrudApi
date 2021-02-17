d9de15ee62f2785d33fd4e7d8ce3b05be24543c0
Uriel Francisco Libano <uriellibano2009@gmail.com>
2021-02-17 18:38:38 -0300

    Corrected the orthography in UsserController

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -23,7 +23,7 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User user(@PathVariable("id") Long id){  
-        System.out.println("O id e " + id);
+        System.out.println("O id é " + id);
         Optional<User> userFind = users.stream().filter(user -> user.getId() == id).findFirst();
 
         if(userFind.isPresent()){
