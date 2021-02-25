1caf01eeb2a0987e7987023dcde61bf50220273c
Uriel Francisco Libano <uriellibano2009@gmail.com>
2021-02-25 15:15:12 -0300

    Insert search methods into UserController

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springboot.model.User;
+import br.com.springboot.repository.UserRepository;
 
 
 @RestController
@@ -20,29 +22,32 @@ public class UserController {
 
 
     private List<User> users = new ArrayList<>();
+    
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/{id}")
     public User user(@PathVariable("id") Long id){  
         System.out.println("O id é: " + id);
-        Optional<User> userFind = users.stream().filter(user -> user.getId() == id).findFirst();
-
-        if(userFind.isPresent()){
+        Optional<User> userFind = this.userRepository.findById(id);
+        if (userFind.isPresent()){
             return userFind.get();
-        }else{
-            return null;
-        }
-        
+        }return null;
         
     }
     @PostMapping("/")
     public User user(@RequestBody User user){
-        users.add(user);
-        return user;
+        return this.userRepository.save(user);
+        
     }
 
     @GetMapping("/list")
     public List<User> list(){
-        return users;
+        return this.userRepository.findAll();
     }
-    
+    @GetMapping("/list/{id}")
+    public List<User> buscaPorIdMaiorQue(@PathVariable("id") Long id){
+        System.out.println(id);
+        return this.userRepository.buscaPorIdMaiorQue(id);
+    }    
 }
